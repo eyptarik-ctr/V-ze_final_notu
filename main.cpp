49867bdd055cfa30c11e99c83e682eedbf14203b
Tarik Aybey <tarikeyup.aybey45@gmail.com>
2023-11-05 00:13:49 +0300

    vize final

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int vize , final;
+    cin >> vize;
+    final = (60 - (vize * 0.4)) / 0.6;
+    cout <<"minimum alman fereken not: " << final ;
+    return 0;
+}
